bbb82ee758fad62a988c7d1461e4cd68c8df8af9
Colin Douglas <colindouglas@gmail.com>
2018-01-11 22:47:13 -0400

    Added counts of positive, negative, and neutral comments.

--- cbb-reddit-sentiment.R
+++ cbb-reddit-sentiment.R
@@ -5,7 +5,7 @@ library(tidytext)
 redditComments <- read_csv("reddit_comments_scape.csv") %>%
   
   ### Only keep the important columns
-  select(post_date, comment_score, comment, link, structure, post_score, upvote_prop) %>%
+  select(post_date, comment_score, comment, link, URL, structure, post_score, upvote_prop) %>%
   
   ### Set the post_date column to date format
   mutate(post_date = as.Date(post_date, format = "%d-%m-%y")) %>%
@@ -14,46 +14,52 @@ redditComments <- read_csv("reddit_comments_scape.csv") %>%
   unnest_tokens(word, comment) %>%
   
   ### Find the sentiment of each word using the "bing" unigram list
-  left_join(get_sentiments("bing")) %>%
+  left_join(get_sentiments("afinn")) %>%
+  mutate(sentiment = score) %>%
   
-  ### Weight the negative words twice as strongly as the positive
-  mutate(sentiment = case_when(
-    sentiment == "negative" ~ -2,
-    sentiment == "positive" ~  1,
-    TRUE ~ 0)) %>%
+  ### Weight the negative and positive words
+  ### Use this code when using unigram lists that aren't "afinn"
+  # mutate(sentiment = case_when(
+  #   sentiment == "negative" ~ -1,
+  #   sentiment == "positive" ~  1,
+  #   TRUE ~ 0)) %>%
   
   ### Group by the fields that are unique to each comment
-  group_by(post_date, link, post_score, upvote_prop, structure) %>%
+  group_by(post_date, link, post_score, upvote_prop, structure, URL) %>%
   
   ### Attempt to classify each comment as positive (+1) or negative (-1), and count the words in each
-  summarize(sentiment = case_when(
-    sum(sentiment, na.rm = TRUE) > 0 ~  1,
-    sum(sentiment, na.rm = TRUE) < 0 ~ -1,
-    TRUE ~ 0), words = n()) %>%
+  summarize(sentiment = sum(sentiment, na.rm = TRUE), words = n()) %>%
   
   ### Group by the fields that are unique to each thread
-  group_by(post_date, link, post_score, upvote_prop) %>%
+  group_by(post_date, link, post_score, upvote_prop, URL) %>%
   
   ### Calculate the total sentiment of each thread, the average comment length, and the number of comments
-  summarize(sentiment = sum(sentiment), avgCommentLength = mean(words), commentCount = n()) %>%
+  summarize(avgCommentLength = mean(words),
+            totalComments = n(),
+            positiveComments = sum(sentiment > 0),
+            negativeComments = sum(sentiment < 0),
+            neutralComments  = sum(sentiment == 0),
+            totalSentiment = sum(sentiment)) %>%
   
   ### Calculate the sentiment per comment, and a z-score for sentiment
-  mutate(rSentiment = (sentiment / commentCount)) %>%
+  mutate(rSentiment = (totalSentiment / totalComments)) %>%
   mutate(zSentiment = (rSentiment - mean(.$rSentiment))/sd(.$rSentiment)) %>%
   
   ### Drop the rSentiment column
   select(-rSentiment) %>%
   
   ### Arrange by sentiment z-score
-  arrange(desc(sentiment))
+  arrange(desc(zSentiment))
 
 ### Output the data
 write_csv(redditComments, "cbb_reddit_sentiment.csv")
 
+### Dotplot to check for correlations
 redditComments %>%
-  ggplot(aes(x = upvote_prop, y = zSentiment)) + 
+  ggplot(aes(x = upvote_prop, y = sentiment)) + 
   geom_point() + geom_smooth(method = "lm")
 
+### Correlation plot to see what correlates
 redditComments %>%
   group_by() %>%
   select_if(is.numeric) %>%
