0794f445e96e83d0c0de388f91165f20a8ee8f48
Colin Douglas <colindouglas@gmail.com>
2018-01-13 20:55:36 -0400

    Completed script to get thread info directly from reddit API

--- cbb-scape-reddit-api.R
+++ cbb-scape-reddit-api.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 
-# httr bugfix from https://github.com/r-lib/httr/pull/485 -----------------
-
+# Set up httr for reddit API calls ----------------------------------------
+### httr bugfix from https://github.com/r-lib/httr/pull/485
 library(devtools)
-devtools::install_github("r-lib/httr#485", force = TRUE)
+devtools::install_github("r-lib/httr#485")
 library(httr)
 
 oauth <- read_csv("oauth_keys.csv")
@@ -16,7 +16,7 @@ endpoint <- oauth_endpoint(
 )
 
 # 2. Register an application at https://www.reddit.com/prefs/apps
-app <- oauth_app("Test_App", oauth$key, oauth$secret)
+app <- oauth_app("EarwolfScrape", oauth$key, oauth$secret)
 
 # 3b. If get 429 too many requests, the default user_agent is overloaded.
 # If you have an application on Reddit then you can pass that using:
@@ -25,19 +25,79 @@ token <- oauth2.0_token(
   app=app,
   scope = c("read", "modposts"),
   use_basic_auth = TRUE,
-  #  cache = F,
+  cache = F,
   config_init = user_agent("Earwolf Comment Scraper")
 )
 
-#trying to make a call
-#Important! Make sure to put oauth.reddit.com instad of reddit.com!
-request_url <- "https://oauth.reddit.com/r/Earwolf/search?q=Comedy+Bang+Bang"
-
-response <- GET(request_url,
-                user_agent("Testing Oauth with httr"),
-                config(token = token)
-)
-
-response
 
 
+
+SearchEarwolfSubreddit <- function(searchTerm) {
+  
+  ### The initial API call to get search results
+  initialURL <- paste0("https://oauth.reddit.com/r/Earwolf/search.json?q=", searchTerm, "+site%3Aearwolf.com&sort=new&type=link&restrict_sr=TRUE&t=all&raw_json=1&limit=100")
+  
+  ### Perform the search API call
+  response <- GET(initialURL,
+                  user_agent("Earwolf Comment Scraper"),
+                  config(token = token)) %>% content()
+  
+  ### Extract the data from the parsed JSON response into a dataframe
+  threads <- data_frame(
+    title = map(response$data$children, ~ .x$data$title),
+    link = map(response$data$children, ~ .x$data$url),
+    fullname = map(response$data$children, ~ .x$data$name),
+    gilded = map(response$data$children, ~ .x$data$gilded),
+    poster = map(response$data$children, ~ .x$data$author),
+    score = map(response$data$children, ~ .x$data$score),
+    permalink = map(response$data$children, ~ .x$data$permalink),
+    numComments = map(response$data$children, ~ .x$data$num_comments)
+  )
+  
+  ### Print status update
+  searchPage <- 1
+  print(paste("Finished page", searchPage, "of results for", searchTerm))
+  
+  ### Init newThreads for the while loop.
+  newThreads <- data_frame()
+  
+  ### Keep searching until you don't get the max number of search results (100)
+  while((nrow(newThreads) == 100 | nrow(threads) == 100) & searchPage < 10) {
+    ### Count how many pages deep we're going
+    searchPage <- searchPage + 1
+    print(paste("Getting page", searchPage, "of results for", searchTerm))
+    # Pause to respect API rules.
+    Sys.sleep(2) 
+    
+    ### Get the fullname of the last thread in the search results
+    lastThread <- tail(threads$fullname, n = 1L)
+    
+    ### Append the last thread fullname to the search URL
+    nextURL <- paste0(initialURL, "&after=", lastThread)
+    
+    ### Get the next set of search results
+    response <- GET(nextURL,
+                    user_agent("Earwolf Comment Scraper"),
+                    config(token = token)) %>% content()
+    
+    ### Extract the data
+    newThreads <- data_frame(
+      title = map(response$data$children, ~ .x$data$title),
+      link = map(response$data$children, ~ .x$data$url),
+      fullname = map(response$data$children, ~ .x$data$name),
+      gilded = map(response$data$children, ~ .x$data$gilded),
+      poster = map(response$data$children, ~ .x$data$author),
+      score = map(response$data$children, ~ .x$data$score),
+      permalink = map(response$data$children, ~ .x$data$permalink),
+      numComments = map(response$data$children, ~ .x$data$num_comments)
+    )
+    
+    ### Combine the old data with the new
+    threads <- bind_rows(threads, newThreads)
+  }
+  print(paste("Done searching for", searchTerm, "after", searchPage, "pages"))
+  return(threads)
+}
+
+redditThreads <- SearchEarwolfSubreddit("comedy+bang+bang") %>%
+  bind_rows(SearchEarwolfSubreddit("CBB"))
