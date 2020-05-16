76a117bf801cc3413dd47d62c878d77fd92c00b8
Colin Douglas <colindouglas@gmail.com>
2020-05-16 13:28:07 -0300

    Fixed typo

--- update-earwolf.sh
+++ update-earwolf.sh
@@ -1,10 +1,10 @@
 TODAY="date +%Y-w%W"
 
 # Update the dataset from the Earwolf website
-#Rscript scrape-earwolf.R
+Rscript scrape-earwolf.R
 
 # Re-render the Best Of analysis
 Rscript -e "rmarkdown::render(\"README.Rmd\")"
-#git add data/earwolf_podcasts.csv data/earwolf_podcasts.Rda images/cbb-bestof-plot-1.png README.md
-#git commit -m "Automated update for `$TODAY`"
-#git push
+git add data/earwolf_podcasts.csv data/earwolf_podcasts.Rda images/cbb-bestof-plot-1.png README.md
+git commit -m "Automated update for `$TODAY`"
+git push
